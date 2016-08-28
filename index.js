5435d692bbe03cbcbd76b7b3c3d61e7e39e27ba0
Doug Colaizzo <dcolaizzo@gmail.com>
2016-08-27 20:00:43 -0700

    Socket.io chatroom working

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.use(express.static('public'));
+app.get('/', function(req, res) {
+    res.sendfile('public/index.html');
+});
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.post('/join/:id', function(req, res) {
+    io.to(req.params.id).emit('chatroom_begin');
+    res.end();
+});
+
+io.on('connection', function(socket) {
+    var roomId = 'cool_kids';
+    socket.join(roomId);
+    socket.emit('joined_room', roomId)
+});
+
+http.listen(3000, function() {
+    console.log('listening on *:3000');
 });
