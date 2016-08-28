ea874ad29bcac007424d4931786e5a1cc38b98d5
Doug Colaizzo <dcolaizzo@gmail.com>
2016-08-27 17:47:12 -0700

    Introduce Myo vector logging

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # who-might-be-robots
 Our hackathon submission for /hack.
 
+Getting started
+======
+To get started, clone the source, and run:
+
+`
+npm install
+npm start
+`
+
 Hackers
 ======
 Doug Colaizzo
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hello, world");
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
