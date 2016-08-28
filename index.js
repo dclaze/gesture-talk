5a3a8f7bd19dc7404a75dba3ebc48a741d72cc2a
Doug Colaizzo <dcolaizzo@gmail.com>
2016-08-28 00:09:56 -0700

    Updated logic for generating room ids

--- index.js
+++ index.js
@@ -10,15 +10,41 @@ app.post('/join/:id', function(req, res) {
     res.end();
 });
 
+function roomIdExists(roomId) {
+    return io.sockets.adapter.rooms.hasOwnProperty(roomId);
+}
+
+function generateId() {
+    var id = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    for (var i = 0; i < 6; i++)
+        id += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return id;
+}
+
+function speechifyId(idStr) {
+    return idStr.replace(/[a-zA-Z]/g, "$& ").replace(/[0-9]/g, "$& ")
+}
+
 io.on('connection', function(socket) {
     socket.on('start_room', function() {
-        var roomId = "A.B.C. 1 2 3";
+        var roomId;
+        do {
+            roomId = generateId();
+        }
+        while (roomIdExists(roomId));
+
         socket.join(roomId);
-        socket.emit('joined_room', roomId)
+        socket.emit('joined_room', {
+            roomId: roomId,
+            speechifiedId: speechifyId(roomId)
+        });
     });
 });
 
 var port = (process.env.PORT || 5000);
 http.listen(port, function() {
-    console.log('listening on *:3000');
+    console.log('listening on *:', port);
 });
